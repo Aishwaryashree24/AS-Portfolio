caf8129ed93e6f2161267cc101b8cf10f19c3bca
Aishwaryashree24 <aishwaryashree826@gmail.com>
2024-11-19 12:13:11 +0530

    resolve merge conflicts

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export",
+    output: "export",
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
+    basePath: '/AS-Portfolio',
 };
 
 export default nextConfig;
